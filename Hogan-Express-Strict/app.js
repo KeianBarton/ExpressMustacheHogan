f884b9a544197a622746079724b8c1ef53dc8d25
KeianBarton <Keian.Barton@civica.co.uk>
2018-07-16 12:27:13 +0100

    Added to mustache templating examples

--- Hogan-Express-Strict/app.js
+++ Hogan-Express-Strict/app.js
@@ -1,4 +1,19 @@
 const config = require('./config');
+const data = require('./data');
+data.lambdas = {
+    boldAndUpper: (text) => { return `<b>${text.toUpperCase()}</b>` },
+    reverseString: (text) => { return text.split("").reverse().join(""); }
+    /*
+    data.renderTextInCoolWayFunc = function() {
+        return function(text, render) {
+            // Lambdas in Hogan no longer require the 'render' funciton
+            // use the hogan-express-strict method instead - see above
+            return '<b>' + render(text.toUpperCase()) + '</b>';
+        }
+    };
+    */
+};
+
 const express = require('express');
 const router = express.Router();
 
@@ -13,7 +28,7 @@ app.engine('html', require('hogan-express-strict'));
 
 app.get('/', (req, res) => {
     res.locals = { name : 'Keian' };
-    res.render('template', { partials : { partial2: 'partials/partial2' } });
+    res.render('template', data);
 });
 
 app.listen(app.settings.port, () => {
